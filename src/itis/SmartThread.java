186ef148a39f0da5852dc5517b091c97d6311082
veresen <lolmariya@gmail.com>
2017-04-06 13:49:11 +0400

    Add files via upload

--- src/itis/SmartThread.java
+++ src/itis/SmartThread.java
@@ -6,13 +6,15 @@ public class SmartThread extends Thread {
 	
 	HashMap<String, ListOfProbabilities> matrix = new HashMap<String, ListOfProbabilities>(); 
 	HashMap<String,Double> vector = new HashMap<String,Double>();
+	HashMap<String,Double> originvector = new HashMap<String,Double>();
 	int start=0; int end=0;
 	String[] order = new String[vector.keySet().size()];
 	
-	public SmartThread(HashMap<String,Double> vector, HashMap<String, ListOfProbabilities> matrix, int start, int end, String[] order) {
+	public SmartThread(HashMap<String,Double> originvector, HashMap<String,Double> vector, HashMap<String, ListOfProbabilities> matrix, int start, int end, String[] order) {
 		// TODO Auto-generated constructor stub
 		this.matrix = matrix;
 		this.vector = vector;
+		this.originvector = originvector;
 		this.start = start;
 		this.end = end;
 		this.order = order;
@@ -24,18 +26,18 @@ public class SmartThread extends Thread {
         try {
             for (int i=start; i<=end; i++) {
             	String vectorkey = order[i];
-            	double old = vector.get(vectorkey);
+            	double old = originvector.get(vectorkey);
     			double sum = 0.15;
     			for (String matrixkey: matrix.keySet()) {
     				if (matrix.get(matrixkey).contains(vectorkey)) {
     					//вот тут умножаем (и сразу в сумму)
-    					sum = sum + 0.85*(matrix.get(matrixkey).get(vectorkey) * vector.get(vectorkey));
+    					sum = sum + 0.85*(matrix.get(matrixkey).get(vectorkey) * originvector.get(vectorkey));
     				}
     			}
     			//if (sum > old) sum = sum - old;
     			vector.put(vectorkey, sum);
             }
-            Thread.sleep(5000);
+            Thread.sleep(50);
            // System.out.println("Smart Thread "+start);
         } catch(InterruptedException i_ex) {
             //System.out.println();
